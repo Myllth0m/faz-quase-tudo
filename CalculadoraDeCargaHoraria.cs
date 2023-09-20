30cffe491fa7d88313eeefbcf00945df2442cf8d
Milton Daniel Yama Ribera <miltondyama@gmail.com>
2023-09-20 11:09:44 -0400

    Adicionar arquivos de projeto.

---
+++ CalculadoraDeCargaHoraria.cs
@@ -0,0 +1,17 @@
+﻿//string InicioDeExpediente = "07:00", FimDeExpediente = "18:00";
+
+//TimeSpan horasSemanais = CalcularHorasSemanais(InicioDeExpediente, FimDeExpediente);
+
+//static TimeSpan CalcularHorasSemanais(string inicioDeExpediente, string fimDeExpediente)
+//{
+//    TimeSpan inicio = TimeSpan.Parse(inicioDeExpediente);
+//    TimeSpan fim = TimeSpan.Parse(fimDeExpediente);
+//    TimeSpan horaDiaria = fim.Subtract(inicio);
+
+//    Console.WriteLine($"horas diarias: {horaDiaria.TotalHours}");
+
+//    return horaDiaria * 5;
+//}
+
+//Console.WriteLine($"horas semanais: {horasSemanais.TotalHours}");
+//Console.WriteLine($"horas mensais: {(horasSemanais * 4).TotalHours}");
---
+++ InterpolarDataEmString.cs
@@ -0,0 +1,5 @@
+﻿//DateTime dataEHoraAtual = DateTime.Now;
+
+//string dataEHoraFormatada = String.Concat(dataEHoraAtual.ToString("dd/MM/yyyy"), " às ", dataEHoraAtual.ToString("HH:mm"));
+
+//Console.WriteLine($"data formatada: {dataEHoraFormatada}");
